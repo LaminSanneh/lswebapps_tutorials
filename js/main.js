8748a15a07070b72450652587576ee99549feb19
Lamin Sanneh <lamin.evra@gmail.com>
2016-01-16 17:18:46 +0000

    tutorial initial commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+var TodoComponent = Vue.extend({
+    data: function(){
+        return {
+            inEditMode: false
+        }
+    },
+    props: ['todo'],
+    template: '<div><span v-on:click="clicked" v-show="!inEditMode">{{ todo.title }}</span><input v-on:keyup.enter="saved" v-model="todo.title" v-show="inEditMode" /> </div>',
+    methods: {
+        clicked: function(){
+            this.inEditMode = true;
+        },
+        saved: function(){
+            this.inEditMode = false;
+        }
+    }
+});
+
+Vue.component('todo-component', TodoComponent);
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue Todo App',
+        someLink: 'http://www.google.com',
+        todoText: '',
+        todos: [
+            { title: 'Todo 1' },
+            { title: 'Todo 2' },
+            { title: 'Todo 3' },
+            { title: 'Todo 4' },
+            { title: 'Todo 5' }
+        ],
+        showTodos: true,
+        count: 1
+    },
+    methods: {
+        createTodo: function () {
+            var todoText = this.todoText.trim();
+            if(todoText){
+                this.todos.push({ title: todoText });
+                this.todoText = '';
+            }
+        },
+        clearTodo: function () {
+            this.todoText = '';
+        },
+        increaseCounter: function () {
+            this.count = this.count+1;
+        },
+        removeTodo: function (index) {
+            this.todos.splice(index, 1);
+        }
+    }
+});
